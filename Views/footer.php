eef21e7d2b3dd63117062738d4a59f315a22619c
DargyLobo <dargijuli@gmail.com>
2022-03-28 18:25:12 -0500

    avances

---
+++ Views/footer.php
@@ -0,0 +1,9 @@
+<section>
+		<?php require('./routing.php'); ?>
+</section>
+<div style="background-color: #74749E; width: 100%; height: 90px; ">
+  <!-- div con color --> 
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ Views/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=}, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+  <div style="background-color: #74749E; width: 100%; height: 80px;">
+    <div class="container">
+      <ul class="nav">
+        <li class="nav-item m-3">
+        <div>
+      <a href="?controller=cita&&action=add" > <button type="button"class="btn btn-secondary btn-lg"  style="background-color: #74749E; height: 50px;">Agregar citas</button> </a>
+    </div>
+        </li>
+        <li class="nav-item m-3">
+        <div >
+      <a href="?controller=cita&&action=show" > <button type="button" class="btn btn-secondary btn-lg" style="background-color: #74749E; height: 50px;">Ver citas</button> </a>
+    </div>        </li>
+      </ul>
+    </div>
+  </div>
+</body>
