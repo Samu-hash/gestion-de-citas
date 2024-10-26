ab85c126369199b7bc71d827e3cb8772fcdee9e6
Samuel Alvarado <64099845+Samu-hash@users.noreply.github.com>
2024-10-25 22:48:33 -0600

    Update conexion.php

--- Config/conexion.php
+++ Config/conexion.php
@@ -13,15 +13,16 @@ class Database {
     }
     
     public static function conectar () {
-        
-        $hostname = 'us-cdbr-east-05.cleardb.net';
-        $database = 'heroku_734ef63ccdc2de6';
-        $username = 'b42d821ea516bb';
-        $password = '33aa38ba';
+
+        $connectDb = 'Database=gestion;Server=pg-server01.postgres.database.azure.com;User Id=kwkeweooph;Password=36gi0OagColj$E$f';
+        $hostname = 'pg-server01.postgres.database.azure.com';
+        $database = 'gestion';
+        $username = 'kwkeweooph';
+        $password = '36gi0OagColj$E$f';
         $charset = 'utf-8';
 
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
-        $pdo = new pdo ("mysql:host={$hostname};dbname={$database};charset{$charset}", $username, $password, $options) ;
+        $pdo = new pdo ("pgsql:host={$hostname};dbname={$database};charset{$charset}", $username, $password, $options) ;
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         return $pdo;
